8182ae33c74988644c11e90ea0dcbc0277bbfcc9
yazid Elhachimi <elhachimiyazid@gmail.com>
2021-09-13 21:34:32 +0200

    feature(finalisation appli avec css final)

--- meteoapi/src/components/formVille.component.jsx
+++ meteoapi/src/components/formVille.component.jsx
@@ -7,9 +7,9 @@ export const Form = (props) =>{
     //renvoi le formulaire permettant de récupérer la ville entrée par l'utilisateur
     return <div className="formulaire">
         <form onSubmit={(e)=>props.recupMeteo(e)}>
-        <input type="text" className="input" name="ville" id="ville" placeholder="Ville" required/>
-        <input type="text" className="input" name="pays" id="pays" placeholder="Pays (ex: FR, US ...)" required/>
-        <button className="submit" name="submit" id="submit" >Meteo par ville</button>
+        <input type="text" className="inputVille" name="ville" placeholder="Ville" required/>
+        <input type="text" className="inputPays" name="pays" placeholder="Pays (ex: FR, US ...)" required/>
+        <button className="submit" name="submit" >Valider</button>
         </form>
     </div>
 }
